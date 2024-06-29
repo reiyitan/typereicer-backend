f8b71fb7b1f9bb979a56736cd10e08999245b2c1
Rei Tan <tanreiyi@gmail.com>
2024-06-29 01:46:56 -0500

    implement get_words

--- routes/words.js
+++ routes/words.js
@@ -1,8 +1,35 @@
 const express = require("express"); 
 const router = express.Router(); 
+const fs = require("fs"); 
+const path = require("path");
+
+const wordFilePath = path.join(__dirname, "..", "data", "words.txt"); 
+const loadWords = () => {
+    const data = fs.readFileSync(wordFilePath, "utf-8");
+    return data.split("\n"); 
+}
+const allWords = loadWords(); 
 
 router.get("/", (req, res) => {
     res.send("words endpoint");
 }); 
+
+router.get("/get_words", (req, res) => {
+    const numWords = req.query.num_words;
+    if (numWords > 50 || numWords > allWords.length) {
+        return res.status(403).json({error: "Limit request to 50 words"});
+    }
+
+    let words = []; 
+    let used = new Set(); 
+    while (words.length < numWords) {
+        let randIndex = Math.floor(Math.random() * allWords.length);
+        if (!used.has(randIndex)) {
+            used.add(randIndex);
+            words.push(allWords[randIndex]);
+        }
+    }
+    return res.json({words});
+});
 
 module.exports = router;
