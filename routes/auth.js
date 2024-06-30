490fbbd03308d56d244d60f41cf2642ee0447eca
Rei Tan <tanreiyi@gmail.com>
2024-06-30 18:11:33 -0500

    implement register

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,19 @@
 const express = require("express"); 
 const router = express.Router();
+const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
+const auth = getAuth();
 
-router.get("/", (req, res) => {
-    res.send("auth endpoint");
+router.post("/register", (req, res) => {
+    const email = req.body.email; 
+    const password = req.body.password;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user; 
+        return res.status(201).json({user});
+    }) 
+    .catch((error) => {
+        return res.status(500).json({error: error.code});
+    })
 });
 
 module.exports = router;
